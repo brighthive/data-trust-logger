8c27e098c6e6d267320ec49ca89be7d12d90b800
Regina Compton <reginafcompton@gmail.com>
2020-01-27 12:57:19 -0500

    chore: Make consistent error messages

--- data_trust_logger/utilities/health_helper.py
+++ data_trust_logger/utilities/health_helper.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 
 from data_trust_logger.utilities import secure_requests
@@ -7,17 +9,18 @@ def _get_endpoint_status(api_ep: str):
     try:
         response = secure_requests.secure_get(api_ep)
         status = response.status_code
+        last_accessed = datetime.utcnow().strftime('%Y-%m-%d %H:%M')
     except Exception:
-        status = 'API error'
+        status = 503
     
-    return status
+    return status, last_accessed
 
 def _get_endpoint_record_count(engine: object, endpoint: str):
     try:
         result = engine.execute(f"SELECT COUNT(*) from {endpoint}")
         count, = result.fetchone()
     except Exception:
-        count = 'Database error'
+        count = -1
     
     return count
 
@@ -29,13 +32,14 @@ def generate_endpoints_blob(engine: object, api_url: str, endpoints: list, mappi
         except KeyError:
             tablename = endpoint
         
-        status = _get_endpoint_status(f"{api_url}/{endpoint}")
+        status, last_accessed = _get_endpoint_status(f"{api_url}/{endpoint}")
         count = _get_endpoint_record_count(engine, tablename)
         
         endpoints_blob['endpoints'].append({
             'endpoint': endpoint,
             'record_count': count,
-            'endpoint_health': status
+            'endpoint_health': status,
+            'last_accessed': last_accessed
         })
     
     return endpoints_blob
