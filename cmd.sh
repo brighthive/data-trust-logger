ef8c17b0ad7f986c9adc257d1eb427e33aa0e533
Regina Compton <reginafcompton@gmail.com>
2020-02-04 16:52:53 -0500

    chore: Use corrent variable

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,9 @@
 
 if [ "$APP_ENV" == "DEVELOPMENT" ] || [ -z "$APP_ENV" ]; then
     python health_audit_runner.py &
-    DATA_MODEL_MANAGER_PID=$!
+    HEALTH_AUDIT_PID=$!
     gunicorn -w 4 -b 0.0.0.0:8002 wsgi:app --reload --worker-class gevent
-    trap "kill -9 $DATA_MODEL_MANAGER_PID" EXIT
+    trap "kill -9 $HEALTH_AUDIT_PID" EXIT
 else
     MAX_RETRIES=5
     WORKERS=4
