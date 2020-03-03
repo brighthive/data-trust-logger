c97b72d1d97a34f524e0ff0b57618dd28993501d
Regina Compton <reginafcompton@gmail.com>
2020-03-03 15:31:35 -0500

    chore: Copy edit

--- data_trust_logger/utilities/basic_logger.py
+++ data_trust_logger/utilities/basic_logger.py
@@ -2,11 +2,9 @@ import logging
 
 
 basic_logger = logging.getLogger("basic_logger")
-basic_logger.setLevel(logging.DEBUG)
 
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
-
 formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)s] %(message)s', datefmt="%a, %d %b %Y %H:%M:%S")
 handler.setFormatter(formatter)
 
