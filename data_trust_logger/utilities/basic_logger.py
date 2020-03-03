f816b087dbef38c9f56386282162d07854ccf494
Regina Compton <reginafcompton@gmail.com>
2020-03-03 15:30:44 -0500

    chore: Do not override root logger

--- data_trust_logger/utilities/basic_logger.py
+++ data_trust_logger/utilities/basic_logger.py
@@ -1,8 +1,13 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO, 
-    format='[%(asctime)s] [%(levelname)s] %(message)s', 
-    datefmt="%a, %d %b %Y %H:%M:%S")
 
-basic_logger = logging.getLogger(__name__)
+basic_logger = logging.getLogger("basic_logger")
+basic_logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(fmt='[%(asctime)s] [%(levelname)s] %(message)s', datefmt="%a, %d %b %Y %H:%M:%S")
+handler.setFormatter(formatter)
+
+basic_logger.addHandler(handler)
